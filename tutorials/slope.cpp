14fcbfcd1cb87644734a93c26232f7d89001076b
Sophie Vu <svu043@uottawa.ca>
2022-10-04 21:24:38 -0400

    [SV] Add tutorial content.

--- tutorials/slope.cpp
+++ tutorials/slope.cpp
@@ -1,15 +1,20 @@
-// function template
 #include <iostream>
 using namespace std;
-
+ 
 int getSlope(int m, int x, int b) {
-    return (m * x + b);
+    return m * x + b;
 }
 
-int main () {
-  int m = 3;
-  int x = 4;
-  int b = 5;
-
-  cout << "The slope of y = " << m << "*" << x << " + " << b << "is " << getSlope(3, 4, 5);
+int main() {
+    int a, b, c;
+    cout << "Let's calculate slopes with the equation y = mx + b. " << endl;
+    cout << "What is the value of m? ";
+    cin >> a;
+    cout << "What is the value of x? ";
+    cin >> b;
+    cout << "What is the value of b? ";
+    cin >> c;
+    cout << "The slope of the equation is " << getSlope(a, b, c) << ". ";
+    // cin >> d;
+    return 0;
 }
