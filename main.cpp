a15a8efd5b22c44d58d278c3067118d927032de9
Sophie Vu <svu043@uottawa.ca>
2022-12-03 11:30:04 -0500

    [SV] Add main.

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "graph.cpp"
+
+int main() {
+    Graph g = Graph(8);
+	g.add_directed_edge(0, 1);
+	g.add_directed_edge(0, 3);
+	g.add_directed_edge(0, 5);
+	g.add_directed_edge(2, 1);
+	g.add_directed_edge(2, 7);
+	g.add_directed_edge(6, 0);
+	g.add_directed_edge(6, 7);
+	g.add_directed_edge(4, 0);
+	g.add_directed_edge(7, 5);
+
+    if (g.is_cyclic()) {
+        cout << "Can't print. It's a cyclical graph." << endl;
+    } else {
+        g.print();
+    }
+    system("PAUSE");
+    return 0;
+}
